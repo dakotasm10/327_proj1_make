a09c4b3e1b6d7d131053e4f0949c32360f7117c3
dakotasm10 <90425642+dakotasm10@users.noreply.github.com>
2021-09-11 14:09:59 -0400

    Add files via upload
    Couldn't commit in terminal due to authentification error apparently.

---
+++ myfunc.h
@@ -0,0 +1,17 @@
+/*
+ * myfunc.h
+ *
+ *  Created on: Sep 11, 2021
+ *      Author: dakotasm10
+ */
+
+#ifndef MYFUNC_H_
+#define MYFUNC_H_
+#include <string>
+
+string myfunc();
+
+
+
+
+#endif /* MYFUNC_H_ */
